781d65e94d0a7a4ad498ae85ece7632c528c9757
Zeeka32 <Zeeka32@gmail.com>
2022-05-07 03:17:32 +0200

    testing github functionalities

---
+++ A2_P1/main.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+template <class T>
+class stack{
+private:
+    struct node{
+        T value;
+        node *next;
+        node *previous;
+    };
+    node *head, *tail;
+    int size = 0;
+public:
+    stack() { head = tail = NULL; }
+
+    int getSize() { return size; }
+
+    void push(T element){
+
+        if(size == 0){
+            node *temp = new node;
+            temp->value = element;
+            head = tail = temp;
+            size++;
+        }
+
+        node *temp = new node;
+        temp->value = element;
+        tail->next = temp;
+        temp->previous = tail;
+        tail = temp;
+        size++;
+    }
+
+    T pop() {
+        if(size == 0)
+            throw "Empty stack exception";
+
+        T temp = tail->value;
+        tail = tail->previous;
+        delete tail->next;
+        size--;
+        
+        return temp;
+    }
+
+
+};
+
+int main(void){
+    stack<char> x;
+
+    char y = 'y';
+    char z = 'e';
+    char t = 'h';
+
+    x.push(y);
+    x.push(z);
+    x.push(t);
+
+    while(x.getSize()){
+        cout << x.pop();
+    }
+}
