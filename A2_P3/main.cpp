70c15a5b50719ae81162d050a17ccd151267faa1
Kyuubei315 <mustafakamel315@gmail.com>
2022-05-08 09:37:36 +0300

    small updates

--- A2_P3/main.cpp
+++ A2_P3/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+template <class T>//template
 class queue{
 private:
     struct node{
@@ -41,9 +42,10 @@ public:
     bool isEmpty() { return size == 0; }
 };
 
+template <class T>
 class stack{
 private:
-    queue q;
+    queue  <T> q;
 public:
 
     void push(int element){
@@ -65,13 +67,40 @@ public:
 
     int top() { return q.front(); }
 
+    bool isEmpty (void)//added for testing
+    {
+        if (q.isEmpty())
+            return true;
+        else 
+            return false;
+    }
+
 };
 
 int main(void){
-
-    stack x;
+    //providing main
+    cout << "test 1\n";
+    stack <int> x;
     x.push(1);
     x.push(2);
     x.push(3);
+    x.push(4);
+    while (!x.isEmpty()){
+        cout << "stack top :" << x.top() << '\n';
+        x.pop();
+    }
+    try{x.pop();}
+    catch (string e){cout << e << endl;}
 
+    cout << endl;
+
+    cout << "test 2\n";
+    x.push(8);
+    x.push(7);
+    x.push(6);
+    x.push(2);
+    while (!x.isEmpty()){
+        cout << "stack top :" << x.top() << '\n';
+        x.pop();
+    }
 }
