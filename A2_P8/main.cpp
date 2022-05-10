9d00aa8927fefa5e568b36175599d89b6622b88f
Zeeka32 <Zeeka32@gmail.com>
2022-05-10 07:53:49 +0200

    problem 8 complete missing 3 test cases

--- A2_P8/main.cpp
+++ A2_P8/main.cpp
@@ -22,15 +22,60 @@ void post_order_traverse (TreeNode *node)
     }
 }
 
-void printPostOrder(string preOrder, string inOrder){
+int findIndex(string inOrder, char target){
+
+    for(int i = 0; i < inOrder.length(); i++)
+        if(inOrder[i] == target)
+            return i;
+        
+    return -1;
+}
+
+TreeNode* insert(string preOrder, string inOrder, int &preIndex, int inStart, int inEnd){
+
+    if(inStart > inEnd)
+        return nullptr;
+
+    TreeNode *node = new TreeNode(preOrder[preIndex]);
+
+    int index = findIndex(inOrder, preOrder[preIndex]);
+
+    preIndex++;
+
+    node->left = insert(preOrder, inOrder, preIndex, inStart, index - 1);
+    node->right = insert(preOrder, inOrder, preIndex, index + 1, inEnd);
+
+    return node;
     
 
+}
+
+void printPostOrder(string preOrder, string inOrder){
+    
+    int preIndex = 0;
+
+    TreeNode *root = insert(preOrder, inOrder, preIndex, 0, inOrder.length() - 1);
 
     post_order_traverse(root);
+
+
 }
 
 int main (void)
 {
 
+    //test case1
+    string preOrder = "ABFGC", inOrder = "FBGAC";
+    cout << "PreOrder: " << preOrder << " InOrder: " << inOrder << "\n";
+    cout << "Post Order: ";
+    printPostOrder(preOrder, inOrder);
+    cout << "\n\n";
+
+    //test case2
+    preOrder = "ABDHECFGI", inOrder = "DHBEAFCGI";
+    cout << "PreOrder: " << preOrder << " InOrder: " << inOrder << "\n";
+    cout << "Post Order: ";
+    printPostOrder(preOrder, inOrder);
+    cout << "\n\n";
     return 0;
 }
