4f14c31da93e8155715ba5fff8eedbf759f707f8
Kyuubei315 <mustafakamel315@gmail.com>
2022-05-15 20:56:44 +0300

    bigger main + tested

--- A2_P9/A2_P9.cpp
+++ A2_P9/A2_P9.cpp
@@ -50,7 +50,15 @@ void freeMemory(TreeNode *node){
 }
 
 int main() {
-    
+
+    /*
+             1
+            / \
+           /   \
+          4     2
+           \   / 
+            5 3   
+    */
     TreeNode *root = new TreeNode(1);
     root->left = new TreeNode(4);
     root->right = new TreeNode(2);
@@ -60,6 +68,14 @@ int main() {
     isFoldable(root) ? cout << "YES\n" : cout << "NO\n";
 
 
+    /*
+             1
+            / \
+           /   \
+          2     3
+         / \     \  
+        4   6     5
+    */
     TreeNode *root2 = new TreeNode(1);
     root2->left = new TreeNode(2);
     root2->right = new TreeNode(3);
@@ -68,10 +84,76 @@ int main() {
     root2->right->right = new TreeNode(5);
 
     isFoldable(root2) ? cout << "YES\n" : cout << "NO\n";
+
+    /*
+             1
+            / \
+           /   \
+          2     3
+         / \   / \  
+        4   6 1   5
+    */
+    TreeNode *root3 = new TreeNode(1);
+    root3->left = new TreeNode(2);
+    root3->right = new TreeNode(3);
+    root3->left->left = new TreeNode(4);
+    root3->left->right = new TreeNode(6);
+    root3->right->right = new TreeNode(5);
+    root3->right->left = new TreeNode(5);
+
+    isFoldable(root3) ? cout << "YES\n" : cout << "NO\n";
+
+    /*
+             1
+            / \
+           /   \
+          2     3
+         / \   / \  
+        4   6 1   5
+       /           \
+      1             4
+    */
+    TreeNode *root4 = new TreeNode(1);
+    root4->left = new TreeNode(2);
+    root4->right = new TreeNode(3);
+    root4->left->left = new TreeNode(4);
+    root4->left->right = new TreeNode(6);
+    root4->right->right = new TreeNode(5);
+    root4->right->left = new TreeNode(5);
+    root4->right->right->right = new TreeNode(4);
+    root4->left->left->left = new TreeNode(1);
+
+    isFoldable(root4) ? cout << "YES\n" : cout << "NO\n";
     
+
+    /*
+             1
+            / \
+           /   \
+          2     3
+         / \   / \  
+        4   6 1   5
+       /         /
+      1         4   
+
+    */
+    TreeNode *root5 = new TreeNode(1);
+    root5->left = new TreeNode(2);
+    root5->right = new TreeNode(3);
+    root5->left->left = new TreeNode(4);
+    root5->left->right = new TreeNode(6);
+    root5->right->right = new TreeNode(5);
+    root5->right->left = new TreeNode(5);
+    root5->right->right->left = new TreeNode(4);
+    root5->left->left->left = new TreeNode(1);
+
+    isFoldable(root5) ? cout << "YES\n" : cout << "NO\n";
 
     freeMemory(root);
     freeMemory(root2);
+    freeMemory(root3);
+    freeMemory(root4);
+    freeMemory(root5);
 
     return 0;
 }
