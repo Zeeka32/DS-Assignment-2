e5f307b4e405cfc635d5eb6d473bce9ce65d2d17
Zeeka32 <Zeeka32@gmail.com>
2022-05-15 22:07:18 +0200

    BETTER SOLUTTIIONNN

--- A2_P10/A2_P10.cpp
+++ A2_P10/A2_P10.cpp
@@ -1,174 +1,98 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
 using namespace std;
 
-template<class T>
-struct Node {
-private:
-    T val;
-    Node *parent;
-    Node *right;
-    Node *left;
-public:
-    Node() {
-        left = nullptr;
-        right = nullptr;
-        parent = nullptr;
-    }
-
-    Node(T v) {
-        val = v;
-        left = nullptr;
-        right = nullptr;
-        parent = nullptr;
-    }
-
-    Node(const T &v, Node *l, Node *r) {
-        val = v;
-        left = new Node<T>(l);
-        right = new Node<T>(r);
-        parent = nullptr;
-    }
-
-    T getVal() { return val; }
-
-    void setVal(T v) { val = v; }
-
-    Node *getParent() { return parent; }
-
-    Node *getRight() { return right; }
-
-    void setRight(Node<T> *node) { right = node; }
-
-    Node *getLeft() { return left; }
-
-    void setLeft(Node<T> *node) { left = node; }
-
-    void setParent(Node<T> *node) { parent = node; }
-
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+    right(right) {}
 };
 
-template<class T>
-class BST {
-private:
-    Node<T> *root;
-public:
-    vector<T> v;
+TreeNode* insert(TreeNode *node, int element){
 
-    BST() { root = nullptr; }
-
-    BST(const T &v) { root = new Node<T>(v); }
-
-    bool empty() { return root == nullptr; }
-
-    T find(const T x) {
-        Node<T> *p = root;
-        while (p != nullptr) {
-            if (x == p->getVal())
-                return p->getVal();
-            (x < p->getVal()) ? p = p->getLeft() : p = p->getRight();
-        }
-        return -1000000000;
+    if(node == NULL){
+        node = new TreeNode(element);
+        return node;
     }
 
-    void insert(T x) {
-        if (empty())
-            root = new Node<T>(x);
-        else {
-            Node<T> *p = root, *prev, *temp = new Node<T>(x);
-            while (p != nullptr) {
-                prev = p;
-                (x < p->getVal()) ? p = p->getLeft() : p = p->getRight();
-            }
-            (x < prev->getVal()) ? prev->setLeft(temp) : prev->setRight(temp);
-            temp->setParent(prev);
-        }
+    if(element > node->val)
+        node->right = insert(node->right, element);
+    else
+        node->left = insert(node->left, element);
+
+    return node;
+}
+
+void in_order_traverse (TreeNode *node, vector<int> &dfs)
+{
+    if (node != NULL){
+        in_order_traverse(node->left, dfs);
+        dfs.push_back(node->val);
+        in_order_traverse(node->right, dfs);
     }
+}
 
-    void dfs(Node<T> *x) {
-        if (x == nullptr) return;
-        v.push_back(x->getVal());
-        dfs(x->getLeft());
-        dfs(x->getRight());
-    }
+int find_Kth_Smallest_Sum(TreeNode *node, int k) {
+    int sum = 0;
+    vector<int> dfs;
+    in_order_traverse(node, dfs);
+    for(int i = 0; dfs[i] <= dfs[k - 1]; i++)
+        sum+= dfs[i];
 
-    long long sum;
+    return sum;
+}
 
-    T findKthSmallest(int k) {
-        sum = 0;
-        dfs(root);
-        sort(v.begin(), v.end());
-        for (int i = 0; i < (int) v.size() && k; ++i) {
-            if (v[i] != v[i + 1])
-                k--;
-            sum += v[i];
-        }
-        return sum;
-    }
+int main (void) {
 
-    void clear(Node<T> *x) {
-        if (x == nullptr) return;
-        clear(x->getLeft());
-        clear(x->getRight());
-        delete x;
-    }
-
-    void clear() {
-        clear(root);
-    }
-
-    ~BST() {
-        clear();
-    }
-
-};
-
-int main() {
-    int k;
-    cin >> k;
-    BST<int> bst1(54), bst2, bst3, bst4, bst5;
-    bst1.insert(51);
-    bst1.insert(49);
-    bst1.insert(52);
-    bst1.insert(75);
-    bst1.insert(74);
-    bst1.insert(85);
-    cout << bst1.findKthSmallest(k) << endl;
+    int k; cin >> k;
+    TreeNode *root = new TreeNode(54);
+    insert(root, 51);
+    insert(root, 49);
+    insert(root, 52);
+    insert(root, 75);
+    insert(root, 74);
+    insert(root, 85);
+    cout << find_Kth_Smallest_Sum(root, k) << "\n";
 
     cin >> k;
-    bst2.insert(8);
-    bst2.insert(1);
-    bst2.insert(9);
-    bst2.insert(3);
-    bst2.insert(7);
-    cout << bst2.findKthSmallest(k) << '\n';
+    TreeNode *root1 = new TreeNode(8);
+    insert(root1 ,1);
+    insert(root1 ,9);
+    insert(root1 ,3);
+    insert(root1 ,7);
+    cout << find_Kth_Smallest_Sum(root1, k) << '\n';
 
     cin >> k;
-    bst3.insert(1);
-    bst3.insert(2);
-    bst3.insert(3);
-    bst3.insert(4);
-    bst3.insert(5);
-    cout << bst3.findKthSmallest(k) << '\n';
+    TreeNode *root2 = new TreeNode(1);
+    insert(root2 ,2);
+    insert(root2 ,3);
+    insert(root2 ,4);
+    insert(root2 ,5);
+    cout << find_Kth_Smallest_Sum(root2, k) << '\n';
 
     cin >> k;
-    bst4.insert(6);
-    bst4.insert(5);
-    bst4.insert(4);
-    bst4.insert(3);
-    bst4.insert(2);
-    bst4.insert(1);
-    cout << bst4.findKthSmallest(k) << '\n';
+    TreeNode *root3 = new TreeNode(6);
+    insert(root3 ,5);
+    insert(root3 ,4);
+    insert(root3 ,3);
+    insert(root3 ,2);
+    insert(root3 ,1);
+    cout << find_Kth_Smallest_Sum(root3, k) << '\n';
 
 
     cin >> k;
-    bst5.insert(5);
-    bst5.insert(5);
-    bst5.insert(5);
-    bst5.insert(5);
-    bst5.insert(5);
-    bst5.insert(5);
-    cout << bst5.findKthSmallest(k) << '\n';
+    TreeNode *root4 = new TreeNode(5);
+    insert(root4 ,5);
+    insert(root4 ,5);
+    insert(root4 ,5);
+    insert(root4 ,5);
+    insert(root4 ,5);
+    cout << find_Kth_Smallest_Sum(root4, k) << '\n';
+
 
     return 0;
 }
